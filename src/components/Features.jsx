3a07289f8ab56dd29c6af79fbdbf06e783ebb453
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2025-01-28 20:34:21 +0100

    added Features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,8 +2,39 @@ import { useState, useRef } from "react";
 import { TiLocationArrow } from "react-icons/ti";
 
 export const BentoTilt = ({ children, className = "" }) => {
-  const [transformStyle, setTransformStyle] = useState(false);
-  return <div className={className}>{children}</div>;
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect();
+
+    const relativeX = (event.clientX - left) / width;
+    const relativeY = (event.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.5) * 10;
+    const tiltY = (relativeX - 0.5) * -10;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.95, .95, .95)`;
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformStyle("");
+  };
+  return (
+    <div
+      ref={itemRef}
+      className={className}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export const BentoCard = ({ src, title, description }) => {
